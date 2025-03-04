b56b0f52a0cfadd9ad4715587363566e84a5cd70
tolyashka <lushnikovt@gmail.com>
2025-03-04 10:54:04 +0300

    Initial Commit

---
+++ TbankIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TbankIOS
+//
+//  Created by Анатолий Лушников on 04.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
